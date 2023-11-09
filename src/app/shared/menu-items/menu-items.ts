7be877edb57cde392424fd0e5f353796402bfb81
ale <alejandrogtsantos@gmail.com>
2023-11-09 02:08:22 -0600

    add home page

--- src/app/shared/menu-items/menu-items.ts
+++ src/app/shared/menu-items/menu-items.ts
@@ -8,13 +8,13 @@ export interface Menu {
 }
 
 const MENUITEMS = [
-  { state: 'dashboard', name: 'Dashboard', type: 'link', icon: 'av_timer' },
-  { state: 'button', type: 'link', name: 'Buttons', icon: 'crop_7_5' },
-  { state: 'grid', type: 'link', name: 'Grid List', icon: 'view_comfy' },
-  { state: 'lists', type: 'link', name: 'Lists', icon: 'view_list' },
+  { state: 'button', type: 'link', name: 'Inicio', icon: 'crop_7_5' },
+  { state: 'grid', type: 'link', name: 'Productos', icon: 'view_comfy' },
+  { state: 'lists', type: 'link', name: 'Comprar', icon: 'view_list' },
+  { state: 'dashboard', name: 'Reportes', type: 'link', icon: 'av_timer' },
   { state: 'menu', type: 'link', name: 'Menu', icon: 'view_headline' },
   { state: 'tabs', type: 'link', name: 'Tabs', icon: 'tab' },
-  { state: 'stepper', type: 'link', name: 'Stepper', icon: 'web' },
+  { state: 'stepper', type: 'link', name: 'Acerca de nostros', icon: 'web' },
   {
     state: 'expansion',
     type: 'link',
